93e65d0f8dabb95ac33d894bbfa1b409e79f9935
Igor Klusek <igorklusek@monogo-001s-macbook-pro-1.home>
2025-12-30 12:50:13 +0100

    FRP-03-fiNAL

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,20 @@
-import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from 'react-router-dom';
 import Home from './ui/Home';
-import Menu, { loader as menuLoader } from './features/menu/Menu';
+import Menu, {
+  loader as menuLoader,
+} from './features/menu/Menu';
 import Cart from './features/cart/Cart';
-import CreateOrder, { action as createOrderAction } from './features/order/CreateOrder';
-import Order, { loader as orderLoader } from './features/order/Order';
+import CreateOrder, {
+  action as createOrderAction,
+} from './features/order/CreateOrder';
+import Order, {
+  loader as orderLoader,
+} from './features/order/Order';
 import AppLayout from './ui/AppLayout';
+import { action as updateOrderAction } from './features/order/UpdateOrder';
 import Error from './ui/Error';
 
 const router = createBrowserRouter([
@@ -36,6 +46,7 @@ const router = createBrowserRouter([
         element: <Order />,
         loader: orderLoader,
         errorElement: <Error />,
+        action: updateOrderAction,
       },
     ],
   },
--- src/features/cart/Cart.jsx
+++ src/features/cart/Cart.jsx
@@ -1,33 +1,18 @@
 import LinkButton from '../../ui/LinkButton';
 import Button from '../../ui/Button';
 import CartItem from './CartItem';
-
-const fakeCart = [
-  {
-    pizzaId: 12,
-    name: 'Mediterranean',
-    quantity: 2,
-    unitPrice: 16,
-    totalPrice: 32,
-  },
-  {
-    pizzaId: 6,
-    name: 'Vegetale',
-    quantity: 1,
-    unitPrice: 13,
-    totalPrice: 13,
-  },
-  {
-    pizzaId: 11,
-    name: 'Spinach and Mushroom',
-    quantity: 1,
-    unitPrice: 15,
-    totalPrice: 15,
-  },
-];
+import EmptyCart from './EmptyCart';
+import { useDispatch, useSelector } from 'react-redux';
+import { clearCart, getCart } from './cartSlice';
 
 function Cart() {
-  const cart = fakeCart;
+  const cart = useSelector(getCart);
+  const username = useSelector(
+    (state) => state.user.username
+  );
+  const dispatch = useDispatch();
+
+  if (!cart.length) return <EmptyCart />;
 
   return (
     <div className="px-4 py-3">
@@ -36,12 +21,12 @@ function Cart() {
       </LinkButton>
 
       <h2 className="mt-7 text-xl font-semibold">
-        Your cart, %NAME%
+        Your cart, {username}
       </h2>
 
       <ul className="divide-y divide-stone-200 border-b mt-3">
         {cart.map((item) => (
-          <CartItem item={item} key={item.key} />
+          <CartItem item={item} key={item.pizzaId} />
         ))}
       </ul>
 
@@ -49,7 +34,12 @@ function Cart() {
         <Button type="primary" to="/order/new">
           Order pizzas
         </Button>
-        <Button type="secondary">Clear cart</Button>
+        <Button
+          type="secondary"
+          onClick={() => dispatch(clearCart())}
+        >
+          Clear cart
+        </Button>
       </div>
     </div>
   );
